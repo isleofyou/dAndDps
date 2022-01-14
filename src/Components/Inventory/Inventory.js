2873200a4b75ad5e8a86018a7b7a4d3e47103977
David Tran <frozenpho@gmail.com>
2022-01-13 20:50:28 -0700

    Update inventory functionality

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -1,16 +1,21 @@
 import React from 'react'; 
 import WeaponsCard from '../WeaponsCard/WeaponsCard';
 
-export default function Inventory(weapons) {
-  return (
-    weapons.map(weapon => {
-      return (
+export default function Inventory({ weapons }) {
+
+  const allWeapons = weapons.map(weapon => {
+    return (
         <WeaponsCard
           name={weapon.name}
           cost={weapon.cost}
           category={weapon.category}
           />
       )
-    })
-  )
+    });
+
+  return (
+    <main className='inventory-container'>
+      {allWeapons}
+    </main>
+  );  
 }
--- src/Components/WeaponsCard/WeaponsCard.js
+++ src/Components/WeaponsCard/WeaponsCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function WeaponsCard(name, cost, category) {
+export default function WeaponsCard({ name, cost, category }) {
   return (
     <div className='weapon-container'>
       <p>{name} {cost} {category}</p>
