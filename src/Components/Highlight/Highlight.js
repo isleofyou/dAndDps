2ca2d86cd1890d23d9d88e7746eef59499d4aee4
David Tran <frozenpho@gmail.com>
2022-01-14 18:17:41 -0700

    Create variable to choose property

--- src/Components/Highlight/Highlight.js
+++ src/Components/Highlight/Highlight.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export default function Highlight(chosenWeapon) {
-  console.log(chosenWeapon)
+  const highlightWeapon = chosenWeapon.chosenWeapon;
+
   return (
     <div className='chosen-weapon'>
-      <p>{chosenWeapon.chosenWeapon.name}</p>
-      <p>{chosenWeapon.chosenWeapon.cost}</p>
-      <p>{chosenWeapon.chosenWeapon.damage_type}</p>
-      <p>{chosenWeapon.chosenWeapon.weight}</p>
-      {chosenWeapon.chosenWeapon.properties && <p>{chosenWeapon.chosenWeapon.properties.join(', ')}</p>}
+      <p>{highlightWeapon.name}</p>
+      <p>{highlightWeapon.cost}</p>
+      <p>{highlightWeapon.damage_type}</p>
+      <p>{highlightWeapon.weight}</p>
+      {highlightWeapon.properties && <p>{highlightWeapon.properties.join(', ')}</p>}
     </div>
   )
 }
