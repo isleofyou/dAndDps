397a057f58e79a63f78e8f8d8d8fe5e71cbb9d51
David Tran <frozenpho@gmail.com>
2022-01-16 11:38:45 -0700

    Update font

--- src/Components/Highlight/Highlight.js
+++ src/Components/Highlight/Highlight.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './Highlight.css';
 
 export default function Highlight(chosenWeapon) {
-  const weaponPics = require.context('../../assets/weaponPics/', true);
   const highlightWeapon = chosenWeapon.chosenWeapon;
   return (
     <div className='chosen-weapon'>
-      <img src={require(`../../assets/weaponPics/${highlightWeapon.slug}.png`)} alt={highlightWeapon.name}/>
+      <img className='weapon-pic' src={require(`../../assets/weaponPics/${highlightWeapon.slug}.png`)} alt={highlightWeapon.name}/>
       <h3>{highlightWeapon.name}</h3>
       <p>Cost: {highlightWeapon.cost}</p>
       <p>Damage type: {highlightWeapon.damage_type}</p>
       <p>Weight: {highlightWeapon.weight}</p>
       {highlightWeapon.properties && <p>Properties: {highlightWeapon.properties.join(', ')}</p>}
-      <Link to='/roll'>
-        <button>Roll</button>
-      </Link>
+      <div className='button-container'>
+        <Link to='/roll'>
+          <button>Roll</button>
+        </Link>
+      </div>
     </div>
   )
 }
