71fb9079a261d1c27e5d7e364dbedff4f97ee547
David Tran <frozenpho@gmail.com>
2022-01-16 12:54:30 -0700

    Update homepage format

--- src/Components/WeaponsTable/WeaponsTable.js
+++ src/Components/WeaponsTable/WeaponsTable.js
@@ -115,22 +115,4 @@ const WeaponsTable = (props) => {
   );
 };
 
-// export default function WeaponsTable() {
-//   return (
-//     <div className="App">
-//       <WeaponsTable
-//         products={[
-//           { id: 1, name: 'Cheese', price: 4.9, stock: 20 },
-//           { id: 2, name: 'Milk', price: 1.9, stock: 32 },
-//           { id: 3, name: 'Yoghurt', price: 2.4, stock: 12 },
-//           { id: 4, name: 'Heavy Cream', price: 3.9, stock: 9 },
-//           { id: 5, name: 'Butter', price: 0.9, stock: 99 },
-//           { id: 6, name: 'Sour Cream ', price: 2.9, stock: 86 },
-//           { id: 7, name: 'Fancy French Cheese 🇫🇷', price: 99, stock: 12 },
-//         ]}
-//       />
-//     </div>
-//   );
-// }
-
 export default WeaponsTable;
